0fbcbb3f11e9f85eff2ad3e55888b1d40ba39d9c
IceTiki <tiki_pufferfish@outlook.com>
2023-04-10 00:57:31 +0800

    对邮件发送的接口进行更新, 对部分命名规范化

--- sendmsg.py
+++ sendmsg.py
@@ -2,6 +2,8 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.utils import formataddr
 from urllib import parse
 import re
 
@@ -12,27 +14,38 @@ import requests  # requests
 class Smtp:
     """Smtp发送类"""
 
-    def __init__(self, host: str, user: str, key: str, sender: str, receivers: list):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        user: str,
+        key: str,
+        sender: str,
+        sender_name: str,
+        use_ssl=True,
+    ):
         """
         :param host: SMTP的域名
+        :param port: SMTP的端口(一般是465或25)
         :param user: 用户名
         :param key: 用户的密钥
         :param sender: 邮件发送者(邮箱)
-        :param receivers: 邮件接收者列表(邮箱)
+        :param sender_name: 发送者名称(可以随便填)
+        :param use_ssl: 是否使用SSL(一般使用SSL的端口是465, 不使用是25)
         """
         self.host = host
+        self.port = port
         self.user = user
         self.key = key
         self.sender = sender
-        self.receivers = receivers
+        self.sender_name = sender_name
+        self.use_ssl = use_ssl
 
     @property
     def __is_config_correst(self):
         # 简单检查邮箱地址或API地址是否合法
-        if type(self.receivers) != list:
-            return 0
-        for item in [self.host, self.user, self.key, self.sender] + self.receivers:
-            if not type(item) == str:
+        for item in [self.host, self.user, self.key, self.sender]:
+            if type(item) != str:
                 return 0
             if len(item) == 0:
                 return 0
@@ -40,24 +53,50 @@ class Smtp:
                 return 0
         return 1
 
-    def send(self, msg, title="no title"):
-        """发送邮件
+    def send(
+        self,
+        receivers: list,
+        msg: str,
+        title: str = "no title",
+        subtype: str = "html",
+        attachments=(),
+    ):  # sourcery skip: remove-unnecessary-cast
+        """
+        发送邮件
+        :param receivers: 邮件接收者列表(邮箱)
         :param msg: 要发送的消息(自动转为字符串类型)
-        :param title: 邮件标题(自动转为字符串类型)"""
+        :param title: 邮件标题(自动转为字符串类型)
+        :param subtype: 邮件类型
+        :params attachment: 附件元组，形式为((blob二进制文件,filename文件名),(blob,filename),...)
+        """
         msg = str(msg)
         title = str(title)
         if not self.__is_config_correst:
             print("邮件配置出错")
             return "邮件配置出错"
-        else:
-            mail = MIMEText(msg, "plain", "utf-8")
-            mail["Subject"] = Header(title, "utf-8")
 
-            smtpObj = smtplib.SMTP()
-            smtpObj.connect(self.host, 25)
-            smtpObj.login(self.user, self.key)
-            smtpObj.sendmail(self.sender, self.receivers, mail.as_string())
-            print("邮件发送成功")
+        mail = MIMEMultipart()
+        # 添加正文
+        mail.attach(MIMEText(msg, subtype, "utf-8"))
+        # 添加标题
+        mail["Subject"] = Header(title, "utf-8")
+        # 添加发送者
+        mail["From"] = formataddr((self.sender_name, self.sender), "utf-8")
+        # 添加附件
+        for att_full in attachments:
+            att = MIMEText(att_full[0], "base64", "utf-8")
+            att["Content-Type"] = "application/octet-stream"
+            att["Content-Disposition"] = f'attachment; filename="{att_full[1]}"'
+            mail.attach(att)
+        # 发送邮件
+        smtp_obj = (
+            smtplib.SMTP_SSL(self.host, self.port)
+            if self.use_ssl
+            else smtplib.SMTP(self.host, self.port)
+        )
+        smtp_obj.login(self.user, self.key)
+        smtp_obj.sendmail(self.sender, receivers, mail.as_string())
+        print("邮件发送成功")
 
 
 class Qmsg:
@@ -98,7 +137,7 @@ class Qmsg:
         else:
             sendtype = "group/" if self.isGroup else "send/"
             res = requests.post(
-                url="https://qmsg.zendee.cn/" + sendtype + self.key,
+                url=f"https://qmsg.zendee.cn/{sendtype}{self.key}",
                 data={"msg": msg, "qq": self.qq},
             )
             return str(res)
@@ -116,38 +155,33 @@ class Pushplus:
     @property
     def __is_config_correct(self):
         # 简单检查邮箱地址或API地址是否合法
-        if not type(self.parameters) == str:
+        if type(self.parameters) != str:
             return 0
-        if not self.parameters:
-            return 0
-        return 1
+        return 1 if self.parameters else 0
 
-    def sendPushplus(self, msg, title):
+    def send_pushplus(self, msg: str, title: str):
+        # sourcery skip: remove-unnecessary-cast
         msg = str(msg)
-        msg = msg.replace("\n", "</br>")
         title = str(title)
-
-        if self.__is_config_correct:
-            if "=" in self.parameters:
-                params = parse.parse_qs(parse.urlparse(self.parameters).path)
-                for k in params:
-                    params[k] = params[k][0]
-                params.update({"title": title, "content": msg})
-            else:
-                params = {
-                    "token": self.parameters,
-                    "title": title,
-                    "content": msg,
-                }
-            headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
-            }
-            res = requests.post(
-                "https://pushplus.hxtrip.com/send", headers=headers, params=params
-            )
-            if res.status_code == 200:
-                return "发送成功"
-            else:
-                return "发送失败"
-        else:
+        msg = msg.replace("\n", "</br>")
+        if not self.__is_config_correct:
             return "pushplus的令牌填写错误，已取消发送！"
+
+        if "=" in self.parameters:
+            params = parse.parse_qs(parse.urlparse(self.parameters).path)
+            for k in params:
+                params[k] = params[k][0]
+            params.update({"title": title, "content": msg})
+        else:
+            params = {
+                "token": self.parameters,
+                "title": title,
+                "content": msg,
+            }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
+        }
+        res = requests.post(
+            "https://pushplus.hxtrip.com/send", headers=headers, params=params
+        )
+        return "发送成功" if res.status_code == 200 else "发送失败"
