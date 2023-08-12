ef4b56345d23a6e8409af55314a1cd5a67b54fff
IceTiki <tiki_pufferfish@outlook.com>
2023-08-12 21:42:15 +0800

    enhance: 利用pathlib简化代码

--- enhance.py
+++ enhance.py
@@ -1,40 +1,43 @@
 # 标准库
-from io import TextIOWrapper
-import os
-import sys
-import imp
-import codecs
-import traceback
+import io as _io
+import os as _os
+import sys as _sys
+import imp as _imp
+import codecs as _codecs
+import traceback as _traceback
+import pathlib as _pathlib
 
 
-def enhance_init(workDir=__file__, python_version_require=0, check_module_list=[]):
+def enhance_init(work_dir=__file__, python_version_require=0, check_module_list=[]):
     """
     :param pythonVersionRequire(int): 最低python所需版本
     :param checkModuleList: 示例: ("requests", "requests_toolbelt", "urllib3", "bs4", "Crypto", "pyDes", "yaml", "lxml", "rsa")
     """
     # ==========检查python版本==========
-    if not (sys.version_info[0] == 3 and sys.version_info[1] >= python_version_require):
+    if not (
+        _sys.version_info[0] == 3 and _sys.version_info[1] >= python_version_require
+    ):
         raise Exception(
             "!!!!!!!!!!!!!!Python版本错误!!!!!!!!!!!!!!\n请使用python3.%d及以上版本，而不是[python %s]"
-            % (python_version_require, sys.version)
+            % (python_version_require, _sys.version)
         )
     # ==========环境变量初始化==========
     try:
         print("==========开始初始化==========")
     except UnicodeEncodeError:
         # 设置默认输出编码为utf-8, 但是会影响腾讯云函数日志输出。
-        sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+        _sys.stdout = _codecs.getwriter("utf-8")(_sys.stdout.detach())
         print("==========开始初始化(utf-8输出)==========")
-    absScriptDir = os.path.dirname(os.path.abspath(workDir))
-    os.chdir(absScriptDir)  # 将工作路径设置为脚本位置
-    if os.name == "posix":
+    script_abs_path = _pathlib.Path(work_dir).absolute().parent
+    _os.chdir(script_abs_path)  # 将工作路径设置为脚本位置
+    if _os.name == "posix":
         # 如果是linux系统, 增加TZ环境变量
-        os.environ["TZ"] = "Asia/Shanghai"
-    sys.path.append(absScriptDir)  # 将脚本路径加入模块搜索路径
+        _os.environ["TZ"] = "Asia/Shanghai"
+    _sys.path.append(script_abs_path)  # 将脚本路径加入模块搜索路径
     # ==========检查第三方模块==========
     try:
         for i in check_module_list:
-            imp.find_module(i)
+            _imp.find_module(i)
     except ImportError as e:  # 腾讯云函数在初始化过程中print运作不正常，所以将信息丢入异常中
         raise ImportError(
             f"""!!!!!!!!!!!!!!缺少第三方模块(依赖)!!!!!!!!!!!!!!
@@ -50,7 +53,7 @@ class Decorators:
             try:
                 return func(*args, **kwargs)
             except Exception:
-                print(traceback.format_exc())
+                print(_traceback.format_exc())
                 return None
 
         return wrapper
@@ -63,10 +66,10 @@ class FileOut:
     close()方法可以还原stdout和stderr
     """
 
-    stdout = sys.stdout
-    stderr = sys.stderr
+    stdout = _sys.stdout
+    stderr = _sys.stderr
     log: str = ""  # 同时将所有输出记录到log字符串中
-    logFile: TextIOWrapper = None
+    logFile: _io.TextIOWrapper = None
 
     @classmethod
     def set_file_out(cla, path: str = None):
@@ -82,10 +85,10 @@ class FileOut:
         # 更新日志文件输出
         if path:
             try:
-                path = os.path.abspath(path)
-                logDir = os.path.dirname(path)
-                if not os.path.isdir(logDir):
-                    os.makedirs(logDir)
+                path = _os.path.abspath(path)
+                logDir = _os.path.dirname(path)
+                if not _os.path.isdir(logDir):
+                    _os.makedirs(logDir)
                 cla.logFile = open(path, "w+", encoding="utf-8")
                 cla.logFile.write(cla.log)
                 cla.logFile.flush()
@@ -101,9 +104,9 @@ class FileOut:
     @classmethod
     def start(cla):
         """开始替换stdout和stderr"""
-        if type(sys.stdout) != cla and type(sys.stderr) != cla:
-            sys.stdout = cla
-            sys.stderr = cla
+        if type(_sys.stdout) != cla and type(_sys.stderr) != cla:
+            _sys.stdout = cla
+            _sys.stderr = cla
         else:
             print("sysout/syserr已被替换为FileOut")
 
@@ -133,5 +136,5 @@ class FileOut:
         if cla.logFile:
             cla.logFile.close()
         cla.log = ""
-        sys.stdout = cla.stdout
-        sys.stderr = cla.stderr
+        _sys.stdout = cla.stdout
+        _sys.stderr = cla.stderr
