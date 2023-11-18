10893c975f248f4e329841b2501fb97c73ea87e0
IceTiki <tiki_pufferfish@outlook.com>
2023-11-18 15:37:47 +0800

    增强LazyImport

--- __init__.py
+++ __init__.py
@@ -1,17 +1,28 @@
 """
 Tiki的常用函数库
 """
+import types as _types
+import typing as _typing
 
 
 class LazyImport:
-    def __init__(self, module_name: str, fromlist: list[str] = tuple()):
+    def __init__(
+        self,
+        module_name: str,
+        fromlist: list[str] = tuple(),
+        assigned_model: None | _types.ModuleType = None,
+    ):
         self.__module_name: str = module_name
-        self.__module = None
+        self.__module: None | _types.ModuleType = assigned_model
         self.__fromlist = fromlist
 
     def __getattr__(self, name: str):
         if self.__module is None:
             self.__module = __import__(self.__module_name, fromlist=self.__fromlist)
+        if name in self.__fromlist:
+            cls = self.__class__
+            ins: _typing.Self = cls(name, assigned_model=getattr(self.__module, name))
+            return ins
         return getattr(self.__module, name)
 
 
